b036c566373feacadbad7930cf8e58c12170ba97
BradleyK <bradleyjkennett@gmail.com>
2022-05-25 11:03:00 -0400

    html add

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+
+
+var apiKey = "9427e8fb1e4c72e3e0458c62e5a629d6";
+var searchByCity = document.querySelector("#search-by-city");
+var searchBtn = document.querySelector("#search-btn");
+var searchHistory = document.querySelector("#search-history");
+var searchHistoryBtn = document.querySelector("#search-history-btn");
+var clearSearchHistoryBtn = document.querySelector("#clear-history-btn");
+var currentWeather = document.querySelector("#current-weather");
+var fiveDayForcast = document.querySelector("#5-day-forcast");
+
+
