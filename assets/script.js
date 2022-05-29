0e69abae1a7a642d140dbf48f414d47f7b52f2a4
BradleyK <bradleyjkennett@gmail.com>
2022-05-29 15:56:28 -0400

    current weather add

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,34 @@
 
 
-var apiKey = "9427e8fb1e4c72e3e0458c62e5a629d6";
-var searchByCity = document.querySelector("#search-by-city");
-var searchBtn = document.querySelector("#search-btn");
-var searchHistory = document.querySelector("#search-history");
-var searchHistoryBtn = document.querySelector("#search-history-btn");
-var clearSearchHistoryBtn = document.querySelector("#clear-history-btn");
-var currentWeather = document.querySelector("#current-weather");
-var fiveDayForcast = document.querySelector("#5-day-forcast");
+// var apiKey = "9427e8fb1e4c72e3e0458c62e5a629d6";
+
+// var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=9427e8fb1e4c72e3e0458c62e5a629d6";
+
+var date = moment().format('dddd, MMM Do.');
+var currentDay = document.getElementById("current-day")
+
+function getCity(){
+    const newCity = document.getElementById("city-input");
+    const cityName = document.getElementById("city-name");  
+    cityName.innerHTML = "--" + newCity.value +"--" 
+
+
+   
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+newCity.value+"&units=imperial&appid=9427e8fb1e4c72e3e0458c62e5a629d6") 
+    .then(response => response.json())
+    .then(function(response){
+        currentDay.innerHTML = date
+        document.getElementById("current-icon").src = "https://openweathermap.org/img/wn/" + response.weather[0].icon +"@2x.png";
+        document.getElementById("current-temp").innerHTML = "Temperature: "+ Number(response.main.temp).toFixed(0) + "°";
+        document.getElementById("current-wind").innerHTML = "Wind: " + Number(response.wind.speed).toFixed(0) + " mph.";
+        document.getElementById("humidity").innerHTML = "Humidity: " + (response.main.humidity) + " %"
+    });
+    
+    
+
+};
+
+
+
 
 
